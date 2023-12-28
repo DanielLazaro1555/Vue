c6761bd967687ad92d4be3efa804602980554c36
danielhuaman <danielsam_m@hotmail.com>
2023-12-27 20:14:40 -0500

    Nuevos cambios

---
+++ mi-primer-vue/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+const name = 'Vue dinamico'
+const stylecolor = 'color:blue'
+const arrayColores = ['blue' , 'red' , 'peru']
+const activo = false
+
+</script>
+
+<template>
+  <h1>Hola {{ name.toUpperCase() }}</h1>
+  <h2>{{arrayColores}}</h2>
+  <h2 :style="`color:${arrayColores}`">Soy de Perú</h2>
+  <h2 v-if="activo">Estoy activo</h2>
+  <p v-if="!activo"> Estoy inactivo</p>
+</template>
+
+<style>
+h1 {
+  color: tomato;
+}
+</style>
